8d51c3edf200b10d83e63ad025ab8be7c8d335fb
zhypar <zhypar28@gmail.com>
2021-10-22 11:50:17 +0600

    heroku commits

--- courses/settings.py
+++ courses/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'courses.wsgi.application'
 
 DATABASES = {
    'default': {
-      'ENGINE': 'django.db.backends.postgresql',
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
       'NAME': config('DB_NAME'),
       'USER': config('DB_USER'),
       'PASSWORD': config('DB_PASSWORD'),
